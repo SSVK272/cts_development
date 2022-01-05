4b906ca1a00ca818f008451aed4b0d8d1e5799b1
Sai Sahith V K <vksaisahith272@gmail.com>
2022-01-05 08:50:39 +0000

    altered all models

--- dbt_project/models/example/my_second_dbt_model.sql
+++ dbt_project/models/example/my_second_dbt_model.sql
@@ -1,29 +1,6 @@
 
 -- Use the `ref` function to select from other models
 
-with source_data as (
+{{ config(materialized='table',schema='dbt_ssvk')}}
 
-    select 1 as id
-    union all
-    select 2 as id
-    union all
-    select 3 as id
-    union all
-    select 4 as id
-    union all
-    select 5 as id
-	union all
-    select 6 as id
-	union all
-    select 7 as id
-	union all
-    select 8 as id
-	union all
-    select 9 as id
-	union all
-    select 10 as id
-
-)
-
-select *
-from source_data
+select * from {{ ref('my_first_dbt_model') }}
