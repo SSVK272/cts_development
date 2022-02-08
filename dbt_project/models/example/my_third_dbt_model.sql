19d67292e55eb58aad8e8596991daea1216eb2e3
saisahithvk <vksaisahith272@gmail.com>
2022-02-08 08:58:11 +0530

    df

--- dbt_project/models/example/my_third_dbt_model.sql
+++ dbt_project/models/example/my_third_dbt_model.sql
@@ -1,3 +1,9 @@
-{{ config(materialized='table') }}
 
-select * from {{ ref('my_second_dbt_model') }}
+{{ config(materialized='table',schema='dbt_ssvk') }}
+
+with source_data_1(id) as (
+    select 1 as id
+    union all
+    select 2 as id
+)
+select d.id from source_data_1 d
